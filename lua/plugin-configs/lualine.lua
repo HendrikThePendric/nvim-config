bcbdeea359130c337826ef8b12cbd6583a625324
Hendrik de Graaf <hendrik@dhis2.org>
2021-12-01 22:14:49 +0100

    chore: truncate branch name and show parent dir

--- lua/plugin-configs/lualine.lua
+++ lua/plugin-configs/lualine.lua
@@ -1,1 +1,38 @@
-require('lualine').setup({})
+function file_name_with_parent_dir(fallbackStr)
+    local str = vim.api.nvim_buf_get_name(0)
+    local level = 0
+    local outpuStr = ''
+    local reversedStr = string.reverse(str)
+
+    for c in reversedStr:gmatch "." do
+        if (c == "/") then
+            level = level + 1
+        end
+        if (level > 1) then
+            break
+        end
+        outpuStr = outpuStr .. c
+    end
+
+    if (outpuStr == '') then
+        return fallbackStr
+    end
+
+    return string.reverse(outpuStr)
+end
+
+require('lualine').setup {
+    sections = {
+        lualine_b = {{
+            'branch',
+            fmt = function(str)
+                return str:sub(1, 16) .. '...'
+            end
+        }},
+        lualine_c = {{
+            'filename',
+            fmt = file_name_with_parent_dir
+        }},
+        lualine_x = {'encoding', 'filetype'}
+    }
+}
