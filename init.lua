8438aa979415525ba3af62a0b7ebc67ebbb5852e
Hendrik de Graaf <hendrik@dhis2.org>
2021-12-04 17:27:40 +0100

    chore: remove todo comments

--- init.lua
+++ init.lua
@@ -2,18 +2,3 @@ require('settings')
 require('keybindings')
 require('plugins')
 require('plugin-configs')
-
--- TODO:
--- Add function signature to LSP (if possible)              DONE
--- Look into replacing null-ls with eslint language server  DOES NOT SEEM NECESSARY
--- lsp-colors                                               DONE
--- vim-styled-jsx (?)                                       FAILED
--- trouble.nvim                                             DONE
--- Some sort Git intergration (telescope plugin / builtin?) TRY BUILT IN FIRST
--- Project intergration (telescope plugin project.nvim)     DONE
--- nvim neoclip                                             DONE
--- ToggleTerm                                               DONE
--- Indentguide                                              DONE
--- Git gutter / git signs                                   
--- colorizer                                                DONE
--- Configure which-key for chosen plugins
